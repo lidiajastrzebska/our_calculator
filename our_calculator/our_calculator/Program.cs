dda02f1ce3be7cc2cb8fdfcd5af5c812d9813c17
lidiajastrzebska <lidiajastrzebska2000@gmail.com>
2022-01-23 16:20:20 +0100

    commit

--- our_calculator/our_calculator/Program.cs
+++ our_calculator/our_calculator/Program.cs
@@ -17,16 +17,44 @@ namespace our_calculator
                 Console.WriteLine("1. Simple Calculator");
                 Console.WriteLine("2. Advanced Calculator");
                 Console.WriteLine("3. TXT");
-                
-              
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (option == 1)
+                {
+                    Console.WriteLine("insert numbers");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int k = Convert.ToInt32(Console.ReadLine());
 
+                    Console.WriteLine("Choose the operation");
+                    Console.WriteLine("1. addition");
+                    Console.WriteLine("2. substraction");
+                    Console.WriteLine("3. multiplication");
+                    Console.WriteLine("4. divide");
+                    int mathOperation = Convert.ToInt32(Console.ReadLine());
+                    if (mathOperation == 1)
+                    {
+                        Console.WriteLine(bul1.Add(n, k));
+                    }
+                    if (mathOperation == 2)
+                    {
+                        Console.WriteLine(bul1.Sub(n,k));
+                    }
+                    
+
+
+                }
                 
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Insert the numbers");
-                Console.ForegroundColor = ConsoleColor.White;
-                int n = Convert.ToInt32(Console.ReadLine());
-                int k = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
 
 
             }
