14388a089da0026d9e4e5395a5a3eb86a8106ff8
lidiajastrzebska <lidiajastrzebska2000@gmail.com>
2022-01-30 12:14:19 +0100

    switch change

--- Our_calc/Program.cs
+++ Our_calc/Program.cs
@@ -17,46 +17,67 @@ class Program
             Console.WriteLine("1. Simple Calculator");
             Console.WriteLine("2. Advanced Calculator");
             Console.WriteLine("3. TXT");
-            int option = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            if (option == 1)
-            {
-                Console.WriteLine("insert numbers");
-                int n = Convert.ToInt32(Console.ReadLine());
-                int k = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Choose the operation");
+            Console.Clear();
+
+            {
+                Console.WriteLine("insert first number");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("insert first number");
+                int k = Convert.ToInt32(Console.ReadLine());
+                int result;
+
+                Console.WriteLine("Choose operation");
                 Console.WriteLine("1. addition");
-                Console.WriteLine("2. substraction");
+                Console.WriteLine("2. subtraction");
                 Console.WriteLine("3. multiplication");
                 Console.WriteLine("4. divide");
-                int mathOperation = Convert.ToInt32(Console.ReadLine());
-                if (mathOperation == 1)
+
+                Console.WriteLine("Choose action");
+                string action = Console.ReadLine();
+
+                switch (action)
                 {
-                    Console.WriteLine(bul1.Add(n, k));
+                    case "1":
+                        result = n + k;
+                        Console.WriteLine("result=" + result);
+                        break;
+                    case "2":
+                        result = n - k;
+                        Console.WriteLine("result=" + result);
+                        break;
+                    case "3":
+                        result = n * k;
+                        Console.WriteLine("result=" + result);
+                        break;
+                    case "4":
+                        result = n / k;
+                        Console.WriteLine("result=" + result);
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+
                 }
-                if (mathOperation == 2)
                 {
-                    Console.WriteLine(bul1.Sub(n, k));
+                    Console.ReadLine();
                 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
